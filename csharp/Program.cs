042a4c7051c2ff4873ca474aa8e1e6f03730c857
Aff1ic7i0n <Aff1ic7i0n@live.com>
2019-09-27 01:32:01 +0300

    Added catch exception and pause after executing code in debug mode, rewrote Items to more readable way.

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -15,24 +15,9 @@ namespace csharp
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
                 new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
                 new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 15,
-                    Quality = 20
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 10,
-                    Quality = 49
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 5,
-                    Quality = 49
-                },
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 49},
 				// this conjured item does not work properly yet
 				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
@@ -40,7 +25,7 @@ namespace csharp
             var app = new GildedRose(Items);
 
 
-            for (var i = 0; i < 31; i++)
+            for (var i = 0; i <= 31; i++)
             {
                 Console.WriteLine("-------- day " + i + " --------");
                 Console.WriteLine("name, sellIn, quality");
@@ -51,6 +36,20 @@ namespace csharp
                 Console.WriteLine("");
                 app.UpdateQuality();
             }
+
+        //Do not close an application in Debug mode after execution
+        #if DEBUG
+            try
+            {
+                //...
+            }
+            finally
+            {
+                Console.WriteLine("Press Enter to close...");
+                Console.ReadLine();
+            }
+        #endif
+
         }
     }
 }
