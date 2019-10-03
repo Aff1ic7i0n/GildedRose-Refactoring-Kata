7df360b5feccf63ad1089be77e4d159780b33a35
Aff1ic7i0n <Aff1ic7i0n@live.com>
2019-10-04 00:09:25 +0300

    Fixed catch exception and if one occurs write it in console. Added saving console output to txt file so it could be used to compare old code output with refactored code output.

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 
 namespace csharp
 {
@@ -9,47 +11,53 @@ namespace csharp
         {
             Console.WriteLine("OMGHAI!");
 
-            IList<Item> Items = new List<Item>{
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20},
-                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49},
-                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 49},
-				// this conjured item does not work properly yet
-				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-            };
-
-            var app = new GildedRose(Items);
-
-
-            for (var i = 0; i <= 31; i++)
-            {
-                Console.WriteLine("-------- day " + i + " --------");
-                Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < Items.Count; j++)
-                {
-                    System.Console.WriteLine(Items[j]);
-                }
-                Console.WriteLine("");
-                app.UpdateQuality();
-            }
-
-        //Do not close an application in Debug mode after execution
-        #if DEBUG
             try
             {
-                //...
+                IList<Item> Items = new List<Item>
+                {
+                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                    new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20},
+                    new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49},
+                    new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 49},
+                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                };
+
+                var app = new GildedRose(Items);
+
+                string fname = DateTime.Now.Ticks + ".txt";
+                File.Create(fname).Close();
+                for (var i = 0; i <= 31; i++)
+                {
+                    Console.WriteLine("-------- day " + i + " --------");
+                    Console.WriteLine("name, sellIn, quality");
+                    for (var j = 0; j < Items.Count; j++)
+                    {
+                        System.Console.WriteLine(Items[j]);
+                        File.AppendAllText(fname, Items[j].ToString() + "\n");
+                    }
+
+                    Console.WriteLine("");
+                    app.UpdateQuality();
+                }
+            }
+
+            // catch exception and output in console if one occurs
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
             finally
             {
-                Console.WriteLine("Press Enter to close...");
-                Console.ReadLine();
+#if DEBUG
+                // hold console window until any key is pressed
+                Console.WriteLine("Press any key to close...");
+                Console.ReadKey();
+#endif
             }
-        #endif
-
         }
     }
 }
