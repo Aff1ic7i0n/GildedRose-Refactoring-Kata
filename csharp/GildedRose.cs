44fed4bba626a38c36f683c7f5379ab223b4d365
Aff1ic7i0n <Aff1ic7i0n@live.com>
2019-10-03 23:57:34 +0300

    Added handlers for diffrent rules and refactored UpdateQuality method.

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -1,16 +1,93 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace csharp
 {
     public class GildedRose
     {
+        // store handlers for special cases
+        private readonly Dictionary<string, Action<Item>> _handlers = new Dictionary<string, Action<Item>>();
+
         IList<Item> Items;
+
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
+            // add handler for Aged Brie
+            _handlers.Add("Aged Brie", item =>
+            {
+                    item.Quality++;
+
+                    if (item.SellIn < 1)
+                    {
+                        item.Quality++;
+                    }
+                    // if quality is more than 50 then 50, we don't allow more than 50
+                    item.Quality = item.Quality > 50 ? 50 : item.Quality;
+            });
+            _handlers.Add("Sulfuras, Hand of Ragnaros", item =>
+            {
+                // nothing should change, so add a day which will be reduced in general handing
+                item.SellIn++;
+            });
+            _handlers.Add("Backstage passes to a TAFKAL80ETC concert", item =>
+            {
+                if (item.SellIn < 1)
+                {
+                    item.Quality = 0;
+                }
+                else
+                {
+                    item.Quality++;
+                    if (item.SellIn <= 10)
+                    {
+                        item.Quality++;
+                    }
+                    if (item.SellIn <= 5)
+                    {
+                        item.Quality++;
+                    }
+                    // if quality is more than 50 then 50, we don't allow more than 50
+                    item.Quality = item.Quality > 50 ? 50 : item.Quality;
+
+                }
+            });
+            _handlers.Add("Conjured Mana Cake", item =>
+            {
+                // just run default handler twice
+                DefaultHandler(item);
+                DefaultHandler(item);
+            });
         }
 
         public void UpdateQuality()
+        {
+            // cycle through all the items and handle them
+            foreach (var item in Items)
+            {
+                // if there's special handler - use it
+                if (_handlers.ContainsKey(item.Name))
+                {
+                    _handlers[item.Name].Invoke(item);
+                }
+                // otherwise use default
+                else
+                {
+                    DefaultHandler(item);
+                }
+                // reduce sellin day
+                item.SellIn--;
+            }
+        }
+        // handler for all non-special cases
+        private void DefaultHandler(Item item)
+        {
+            item.Quality -= item.SellIn > 0 ? 1 : 2;
+            item.Quality = item.Quality < 0 ? 0 : item.Quality;
+        }
+
+        /*
+        public void UpdateQualityOld()
         {
             for (var i = 0; i < Items.Count; i++)
             {
@@ -84,6 +161,7 @@ namespace csharp
                     }
                 }
             }
-        }
+        }*/
+
     }
 }
